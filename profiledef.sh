637999b82e4766185d9a5a1eeab15a1ad2e04ada
Bobby Brockway <bobby.brockway@gmail.com>
2020-12-28 14:45:01 +0000

    profiledef.sh updated

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="bobarch"
+iso_label="BOBARCH_$(date +%Y%m)"
+iso_publisher="Bobby Brockway <https://github.com/bcbrockway>"
+iso_application="BobArch Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
